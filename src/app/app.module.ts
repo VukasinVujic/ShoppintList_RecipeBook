b7e449680202a20988c26816ffa59114614e5fdd
VukasinVujic <vukasinvujic021@gmail.com>
2019-12-30 17:16:58 +0100

    adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +20,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
